4c1ad91320a0c5983884611eeb1deaf383da3f1c
cirko0 <cirko007ivan@gmail.com>
2024-03-24 13:21:51 +0100

    created Navbar component and changed website title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-const App = () => {
-  const a = 1;
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
