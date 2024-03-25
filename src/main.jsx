32af1d5a0e511169a500d99e9651616267f63529
cirko0 <cirko007ivan@gmail.com>
2024-03-25 23:30:31 +0100

    fix for eslint configuration and configured Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
 
+import App from "./App.jsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://84171e93967f745cef61e6a7af9a6efb@o4506968334401536.ingest.us.sentry.io/4506968337874944",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
